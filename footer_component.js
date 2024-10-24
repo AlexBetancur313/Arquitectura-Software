3214c9c744080ea8a3b39332572491e7fd469f58
julianqui1 <julianquintero0111@gmail.com>
2024-10-24 17:44:46 -0500

    Creación de componentes

---
+++ footer_component.js
@@ -0,0 +1,20 @@
+class CustomFooter extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const footer = document.createElement("footer");
+    footer.innerHTML = `© 2025 Taller de Arquitectura de Software Uniremington`;
+
+    footer.style.fontSize = "1.75rem";
+    footer.style.fontWeight = "500";
+    footer.style.backgroundColor = "rgb(2, 44, 34)";
+    footer.style.padding = "10px";
+    footer.style.textAlign = "center";
+    footer.style.color = "white";
+
+    shadow.appendChild(footer);
+  }
+}
+
+customElements.define("custom-footer", CustomFooter);
